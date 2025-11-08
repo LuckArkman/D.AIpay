c6b87d797949a7b14228cf9d5c6d3b56af9aecd0
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2025-11-07 23:22:55 -0300

    up

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,17 +1,36 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "🚀 D.AIpay",
+        Version = "v1",
+        Description = "Descrição da API",
+        Contact = new OpenApiContact
+        {
+            Name = "D.AIpay",
+            Email = "seuemail@dominio.com"
+        }
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.DocumentTitle = "D.AIpay - Swagger Docs";
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "D.AIpay - v1");
+    });
 }
 
 app.UseHttpsRedirection();
